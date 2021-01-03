0f0646f89b8e5c589a1ec18e399199e840dd201b
Ryan Davison <ryancdavison@users.noreply.github.com>
2021-01-03 14:15:53 -0800

    [skip ci]

--- RunCMake.bat
+++ RunCMake.bat
@@ -4,8 +4,8 @@ set BUILD_CONFIG=%1
 set BUILD_DIR=%2
 
 IF NOT DEFINED BUILD_CONFIG (SET BUILD_CONFIG="Debug") 
-IF NOT DEFINED BUILD_DIR (SET BUILD_DIR="build-msvc") 
+IF NOT DEFINED BUILD_DIR (SET BUILD_DIR="build") 
 
 cmake -G "Visual Studio 16 2019" -A x64 -S . -B %BUILD_DIR%
-cd %BUILD_DIR%
-cmake --build . --target HeapDebugger --config %BUILD_CONFIG%
+
+cmake --build %BUILD_DIR% --target HeapDebugger --config %BUILD_CONFIG%
--- RunCMake.sh
+++ RunCMake.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 BUILD_CONFIG=${1:-Debug}
-BUILD_DIR=${2:-build-unix}
+BUILD_DIR=${2:-build}
 
 mkdir -p $BUILD_DIR
 echo "cmake -G \"Unix Makefiles\" -DCMAKE_BUILD_TYPE=$BUILD_CONFIG -S . -B$BUILD_DIR"
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$BUILD_CONFIG -S . -B$BUILD_DIR 
 
-cd $BUILD_DIR
-cmake --build . --config $BUILD_CONFIG
+cmake --build $BUILD_DIR --config $BUILD_CONFIG
